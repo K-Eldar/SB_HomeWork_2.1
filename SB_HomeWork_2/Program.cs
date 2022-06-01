cb558c3fcb370e7067941db7a44757741f75aa7a
User <karaev.e.a@yandex.ru>
2022-06-01 23:29:12 +0600

    with second exersice

--- SB_HomeWork_2/Program.cs
+++ SB_HomeWork_2/Program.cs
@@ -10,9 +10,13 @@ namespace Example_001_Variable
             string FullName = "Иванов Иван Иванович";
             byte age = 55;
             string Email = "ivanov_II@ivanovo.com";
-            double programming_point = 87;
-            double maths_point = 65;
-            double physics_point = 78;
+            double programming_point = 80;
+            double maths_point = 50;
+            double physics_point = 50;
+            double sum_point = programming_point + maths_point + physics_point;
+            double average_point = sum_point / 3;
+            
+           
 
             string pattern = "ФИО: {0} \nВозраст: {1}  \nЭлектронная почта: {2}  \nБаллы по программированию: {3}  \nБаллы по математике: {4}  \nБаллы по физике: {5}";
             
@@ -24,7 +28,15 @@ namespace Example_001_Variable
                 maths_point,
                 physics_point);
 
-           
+            Console.ReadKey();
+
+            Console.WriteLine($"Общий бал по всем предметам: {sum_point} \nСрединий бал: {average_point}");
+
+
+
+
+
+
         }
     }
 }
